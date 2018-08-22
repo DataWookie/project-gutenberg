17afa959305580a3fca01c448ba6dd3896b1841f
Andrew B. Collier <andrew@exegetic.biz>
2018-08-22 04:44:06 +0200

    Initial commit.

---
+++ download-gutenberg.R
@@ -0,0 +1,42 @@
+library(gutenbergr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(purrr)
+
+FOLDER = "gutenberg"
+
+dir.create(FOLDER)
+
+works <- gutenberg_works()
+
+works[1:5,] %>%
+  select(id = gutenberg_id) %>%
+  pwalk(function(id) {
+  cat(sprintf("%d / %d\n", id, nrow(works)))
+    
+    path = file.path(FOLDER, sprintf("%d.txt", id))
+    
+    if (!file.exists(path)) {
+      gutenberg_download(id) %>%
+        pull(text) %>%
+        writeLines(path)
+    }
+  })
+
+# Generate list of the works that we collected.
+#
+collected_works <- list.files(FOLDER, full.names = TRUE) %>%
+  file.info() %>%
+  mutate(
+    gutenberg_id = rownames(.) %>% str_replace_all("[^0-9]", "") %>% as.integer()
+  ) %>%
+  select(gutenberg_id, size) %>%
+  # Exclude empty files (some downloads seem to fail systematically).
+  filter(size != 0)
+
+works <- works %>% inner_join(collected_works)
+
+works %>%
+  select(id = gutenberg_id, author, title, size) %>%
+  write.csv2(file.path(FOLDER, "gutenberg-catalog.csv"), quote = FALSE, row.names = FALSE)
